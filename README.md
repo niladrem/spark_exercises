234201be706cb44f463f76322bc3b9c40a6781df
Anna Urbala <aur@Poro.Poro>
2018-08-09 10:25:11 +0200

    exercise02 - part 1

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 ## Udemy Course - Spark Exercises
+[Taming Big Data with Apache Spark and Python - Hands On!](https://www.udemy.com/taming-big-data-with-apache-spark-hands-on)
 
 ##### Exercise 01
 - Tally up amount spent by customer using Spark
 - Sort your result by amount spent per customer
+
+##### Exercise 02
+- Find similar movies
+- Improve the Quality of Similar Movies
+Used dataset: MovieLens 100k
+[Link to MovieLens Dataset](https://grouplens.org/datasets/movielens/)
---
+++ exercise02/movie-similarities.py
@@ -0,0 +1,79 @@
+import sys
+from pyspark import SparkConf, SparkContext
+from math import sqrt
+
+def loadMovieNames():
+	movieNames = {}
+	f = open('ml-100k/u.item', encoding='ascii', errors='ignore')
+	for line in f:
+		fields = line.split("|")
+		movieNames[int(fields[0])] = fields[1]
+	return movieNames
+
+def parseLine(line):
+	l = line.split()
+	return (int(l[0]), (int(l[1]), float(l[2]))) # userID -> (movieID, rating)
+
+def makePairs(userRatings):
+	ratings = userRatings[1]
+	return ((ratings[0][0], ratings[1][0]), (ratings[0][1], ratings[1][1])) # ((movie1, movie2), (rating1, rating2))
+
+def filterDuplicates(userRatings):
+	ratings = userRatings[1]
+	return ratings[0][0] < ratings[1][0]
+
+def filterScores(pairScores, movieId, scoreThreshold, coOccurenceThreshold):
+	movies = pairScores[0]
+	scores = pairScores[1]
+	return (movies[0] == movieID or movies[1] == movieID) and scores[0] >= scoreThreshold and scores[1] >= coOccurenceThreshold
+
+def cosineSimilarity(ratingPairs):
+	numPairs = 0
+	xx = yy = xy = 0
+	for x, y in ratingPairs:
+		xx += x * x
+		yy += y * y
+		xy += x * y
+		numPairs += 1
+
+	score = 0
+	if xx and yy:
+		score = xy / (float(sqrt(xx) * sqrt(yy)))
+	return (score, numPairs)
+
+def euclideanDistance(ratingPairs):
+	numPairs = 0
+	score = 0
+	for x, y in ratingPairs:
+		score += (x - y) ** 2
+		numPairs += 1
+	score /= 36.0 # normalized value
+	return (sqrt(score), numPairs)
+
+conf = SparkConf().setMaster("local[*]").setAppName("MovieSimilarities")
+sc = SparkContext(conf = conf)
+
+nameDict = loadMovieNames()
+
+data = sc.textFile("ml-100k/u.data")
+ratings = data.map(parseLine)
+
+joinedRatings = ratings.join(ratings).filter(filterDuplicates)
+moviePairRatings = joinedRatings.map(makePairs).groupByKey()
+
+moviePairSimilarities = moviePairRatings.mapValues(cosineSimilarity).cache()
+
+if len(sys.argv) > 1:
+	movieID = int(sys.argv[1])
+	filteredResults = moviePairSimilarities.filter(lambda row: filterScores(row, movieID, 0.97, 50))
+
+	# sort by quality score
+	results = filteredResults.map(lambda pairSimilarities: (pairSimilarities[1], pairSimilarities[0])).sortByKey(ascending = False).take(10)
+	
+	print("Top 10 similar movies for " + nameDict[movieID])
+	for result in results:
+		(similarity, pair) = result
+		similarMovieID = pair[0]
+		if similarMovieID == movieID:
+			similarMovieID = pair[1]
+		print(nameDict[similarMovieID] + "\n\tscore: " + str(similarity[0]) + "\tstrength: " + str(similarity[1]))
